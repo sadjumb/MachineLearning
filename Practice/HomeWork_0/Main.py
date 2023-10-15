c7d0fe23e9c1c7b425029414da12c9b51b045da8
Kirill Makar <kirill-ksm@yandex.ru>
2023-10-15 12:30:33 +0300

    First homework

---
+++ Practice/HomeWork_0/Main.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+# import numpy as np
+
+if __name__ == "__main__":
+    df = pd.read_csv("../../DataSet/weatherAUS.csv")
+    print(f"Shape: {df.shape}")
+    print(
+        f"The presence of missing values {df.isnull().sum().sum() * 100.0 / (len(df) * len(df.keys()))}")
