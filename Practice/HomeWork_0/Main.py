bf15037f72f64032978e3275ef6dcbb1ee2b6432
makar <kirill-ksm@yandex.ru>
2023-11-02 23:31:14 +0300

    HW0

--- Practice/HomeWork_0/Main.py
+++ Practice/HomeWork_0/Main.py
@@ -1,8 +1,11 @@
 
 import pandas as pd
-# import numpy as np
+import numpy as np
 
 if __name__ == "__main__":
     df = pd.read_csv("../../DataSet/weatherAUS.csv")
     print(f"Shape: {df.shape}")
     print(f"The presence of missing values {df.isnull().sum().sum() * 100.0 / (len(df) * len(df.keys()))}")
+    print(f"Rain tomorrow: yes({len(df[df['RainTomorrow'] == 'Yes'])}) / no({len(df[df['RainTomorrow'] == 'No'])}) / NaN({len(df[df['RainTomorrow'].isna()])})")
+    print(f"Rain tomorrow(percent): yes({len(df[df['RainTomorrow'] == 'Yes']) / len(df['RainTomorrow']) * 100}%) / no({len(df[df['RainTomorrow'] == 'No']) / len(df['RainTomorrow']) * 100}%)\
+    NA ~{len(df[df['RainTomorrow'].isna()]) / len(df['RainTomorrow']) * 100}")
