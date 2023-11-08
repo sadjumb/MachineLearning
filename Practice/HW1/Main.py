406b11a4504b7044d773bba60eed1626d7a92b26
makar <kirill-ksm@yandex.ru>
2023-11-08 17:18:52 +0300

    UPD

---
+++ Practice/HW1/Main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    PATH_IMAGES = '/home/makar/Python/MachineLearning/Images/'
+    df = pd.read_csv("../../DataSet/weatherAUS.csv", engine='python')
+
+    fig, ax = plt.subplots()
+    ax.bar(x=['No', 'Yes'], height=[df[df["RainTomorrow"] == "No"]["RainTomorrow"].count(), df[df["RainTomorrow"] == "Yes"]["RainTomorrow"].count()])
+    ax.set_title("Rain Tomorrow")
+    fig.savefig(PATH_IMAGES + 'RainTomorrow.png')
